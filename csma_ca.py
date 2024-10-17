e09bfeab0a8e7d8cb39b2bfbcff1109daac6782e
Tony Bokas <me@tonybokas.com>
2024-10-16 23:04:54 -0400

    Got rough cut of steps A-C implemented

--- csma_ca.py
+++ csma_ca.py
@@ -2,11 +2,24 @@
 
 # Authors: Antonios J. Bokas & Jamie Cookson
 
-import math
-import time
+# To-do:
+# - In the project directions, it shows X values as follows:
+#    X = {985, 789, 1045, 1140}
+#   but I don't understand how he got whole numbers here. The formula he wants
+#   us to use for U and X yield demicals between 0, 1 (also per instructions)
+#
+# - Steps (d) and (e) not implemented yet
+#
+# - Not all nuances of directions implemented yet, many constants still unused
+#
+# - Script not yet tested
 
-from numpy.random import uniform
-import numpy as np
+import time
+from dataclasses import dataclass
+from random import randint
+
+from numpy import log
+from numpy.random import poisson, uniform
 import seaborn as sb
 from matplotlib import pyplot
 
@@ -17,11 +30,11 @@ BW = 12 * 10**6   # bandwidth in bits/sec
 FRAME = 1500 * 8  # frame size in bits
 
 # Slot allocations:
-SLOT = 10                   # slot length in micro sec
-ACK = RTS = CTS = SLOT * 3  # acknowledgment, request- and clear-to-send
-DIFS = SLOT * 4             # distributed interframe space
-SIFS = SLOT * 2             # short interframe space
-CW_MAX = SLOT * 1024        # contention window limit
+SLOT_SIZE = 10                   # slot length in micro sec
+ACK = RTS = CTS = SLOT_SIZE * 3  # acknowledgment, request- and clear-to-send
+DIFS = SLOT_SIZE * 4             # distributed interframe space
+SIFS = SLOT_SIZE * 2             # short interframe space
+CW_MAX = SLOT_SIZE * 1024        # contention window limit
 
 # Simulation:
 SIM_TIME = 10                                   # simulation time in sec
@@ -29,90 +42,180 @@ ARRIVAL_RATE = [100, 200, 300, 500, 700, 1000]  # arrival rate in frames/sec
 
 # VARIABLES
 
-cw = SLOT * 8  # contention window
-throughput = collisions = fairness = 0  # to-do: use None instead?
+throughput = collisions = fairness = 0
 
 
 class App:
-    def __init__(self, station):
-        self.station = station
+    def __init__(self):
+        self.station: Station = None
+        self.frames: list = []
+        self.write_times: list = []
+        self.next_write: float = 0.0
 
-    def buffer_frame(self):
-        pass
+    def generate_traffic(self, rate):
+        # Create uniform distribution as an array:
+        U = uniform(0, 1, rate * 10)
+
+        # Convert the distribution values per Appendix 1 equation and save
+        # updated array values to variable X:
+        X = (-1 / rate) * log(1 - U)
+
+        # Convert to lists for better functionality:
+        self.frames = list(U)
+        self.write_times = list(X)
+        self.next_write = 0.0
+
+    def buffer_frame(self, now):
+        if any(self.frames) and self.next_write <= now:
+            self.station.buffer.append(self.frames.pop(0))
+            self.next_write += self.write_times.pop(0)
 
 
 class Station:
-    def __init__(self, c_domain, access_pt):
-        self.c_domain = c_domain
-        self.access_pt = access_pt
-        self.cw = None
-        self.backoff = None
-        self.awaiting_ack = False
-        self.channel_busy = False
-        self.ack_received = False
+    def __init__(self):
+        self.domain: CollisionDomain = None
+        self.access_pt: AccessPoint = None
+        self.buffer: list = []
+        self.difs: int = DIFS
+        self.nav: int = 0
+        self.backoff: int = randint(0, self.contention_w)
+        self.contention_w: int = SLOT_SIZE * 8
+        self.awaiting_ack: bool = False
 
-    def run(self):
-
-        # Pseudocode of station behvaior:
-        frame = self.check_buffer()
-
-        if not frame:
-            return
-
-        self.sense_channel()
-
-        trys = 0
-        while self.channel_busy:
-            self.select_backoff()
-            self.freeze()
-            self.decrement_backoff()
-            if trys == 10:
-                return 'Timeout'
-
-        self.transmit()
-
-        wait_time = 0
-        while self.awaiting_ack():
-            if self.ack_received:
-                return 'Success'
-            if wait_time == 10:
-                return 'Timeout no ack'
-
-    def check_buffer(self):
-        pass
-
-    def select_backoff(self):
-        pass
-
-    def sense_channel(self, channel_status):
-        self.channel_busy = True
+    def sense_channel(self):
+        if len(self.domain.nav) > 1:
+            return  # to-do: handle collision!
+        elif self.domain.nav:
+            self.nav = self.domain.nav[0]
 
     def freeze(self):
-        pass
-
-    def decrement_backoff(self):
-        self.backoff -= 1
+        if self.nav:
+            self.nav -= 1
+        else:
+            self.difs = DIFS
 
     def transmit(self):
+        self.domain.nav.append(self.buffer[0])
         self.awaiting_ack = True
 
-    def double_cw(self):
+    def double_contention_w(self):
         pass
 
 
 class AccessPoint:
-    def __init__(self, c_domain):
-        self.c_domain = c_domain
+    def __init__(self):
+        self.domain: CollisionDomain = None
+        self.nav: int = 0
 
-    def run(self):
+    def ack(self):
+        # To-do: Ack which station? Probably need to store dict in domain.nav:
         pass
 
-    def ack(self, station):
-        pass
+
+@dataclass
+class CollisionDomain:
+    nav: list = []
+
+    def clear(self):
+        self.nav = []
+
+
+def main():
+    # Create all the entities in the simulation:
+    app_A = App()
+    app_B = App()
+    apps = [app_A, app_B]
+
+    station_A = Station()
+    station_B = Station()
+    stations = [station_A, station_B]
+
+    access_pt = AccessPoint()
+    domain = CollisionDomain()
+
+    # Connect the entities:
+    app_A.station = station_A
+    app_B.station = station_B
+
+    for station in stations:
+        station.access_pt = access_pt
+        station.domain = domain  # to-do: this will change for hidden terminals
+
+    access_pt.domain = domain
+
+    # Create app traffic:
+    for app in apps:
+        app.generate_traffic(ARRIVAL_RATE[0])  # to-do: iterate
+
+    # Set counters:
+    start = time.time()     # start time
+    end = start + SIM_TIME  # start time plus simulation time
+    slot = 0                # slot counter
+
+    print('Simulation start:', start)
+
+    # From my understanding of the directions, we are to time the simulation
+    # but not actually use time to dictate the transmissions. They are just
+    # supposed to flow along according to the slot and then end after 10
+    # seconds. This means that each iteration of the main component of the
+    # script needs to equal one slot. This allows the activities between the
+    # simulated entities the occur in a more realistic way. The only other way
+    # to achieve this parallelism is to have multi-threading, which is not the
+    # objective:
+
+    while True:
+        now = time.time()
+
+        if now >= end:
+            break
+
+        slot += 1
+
+        for app in apps:
+            app.buffer_frame(now - end + SIM_TIME)
+
+        for station in stations:
+            if not any(station.buffer):
+                continue
+
+            if station.difs:
+                station.sense_channel()
+
+            elif station.nav:
+                station.freeze()
+
+            elif station.backoff:
+                station.backoff -= 1
+                station.sense_channel()
+
+            else:
+                station.transmit()
+
+        if len(domain.nav) > 1:
+            # There is a collision. Resume the loop so the stations can
+            # address it:
+            continue
+
+        if domain.nav and not access_pt.nav:
+            access_pt.nav = domain.nav[0]
+
+        if domain.nav:
+            # The frames are transmiting through the domain to the
+            # access point:
+            domain.nav[0] -= 1
+            access_pt.nav -= 1
+
+            if not access_pt.nav:
+                access_pt.ack()
+
+    print('Simulation end:', end)
+
+    # Placeholder, will eventually generate the graphs after each run:
+    # example_poisson()
 
 
 def example_poisson():
-    sample = np.random.poisson(5, 100)
+    sample = poisson(5, 100)
     print('Sample data:')
     print(sample)
 
@@ -120,35 +223,6 @@ def example_poisson():
     plot.set(title='Poisson Distribution', xlabel='event', ylabel='count')
     pyplot.tight_layout()
     pyplot.show()
-
-
-def main():
-    start = time.time() # start time
-    end = start + 10    # start time plus 10 seconds
-
-    # Note: time.time() defaults to seconds, not microseconds
-
-    # This is really just a placeholder for now:
-    print('Simulation start:', start)
-
-    # Selecting the first arrival rate for now. Later we will iterate through
-    # the others:
-    ar = ARRIVAL_RATE[0]
-
-    # 1. Create uniform distribution as an array.
-    # 2. Convert the distribution values per Appendix 1 equation and save
-    #    updated array values to variable X.
-    X = (-1/ar) * np.log(1-uniform(0, 1, ar*10))
-    print(X[:10])  # display first ten elements
-
-    # View the distribution shape:
-    sb.displot(kind='hist', x=X)
-    pyplot.show()
-
-    print('Simulation end:', end)
-
-    # Placeholder, but it will eventually generate the graphs after each run:
-    # example_poisson()
 
 
 if __name__ == '__main__':
