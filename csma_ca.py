cf70366d268080daa2a213f5c1f5b01f4d04fab6
Tony Bokas <me@tonybokas.com>
2024-10-20 16:26:28 -0400

    Refactored

--- csma_ca.py
+++ csma_ca.py
@@ -7,7 +7,6 @@
 # - Script not yet tested
 
 import time
-from dataclasses import dataclass
 from random import randint
 
 import numpy as np
@@ -29,7 +28,8 @@ CW = SLOT_SIZE * 8               # base contention window
 CW_MAX = SLOT_SIZE * 1024        # contention window limit
 
 # Simulation:
-SIM_TIME = 10                                   # simulation time in sec
+# SIM_TIME = 10                                   # simulation time in sec
+SIM_TIME = 1                                   # simulation time in sec
 ARRIVAL_RATE = [100, 200, 300, 500, 700, 1000]  # arrival rate in frames/sec
 
 # VARIABLES
@@ -42,7 +42,15 @@ class App:
         self.station: Station = None
         self.frames: list = []
         self.write_times: list = []
-        self.next_write: float = 0.0
+        self.next_write: int = 0
+
+    def state(self):
+        s = f'{self}:\n'
+        s += f'    station: {self.station}\n'
+        s += f'    next 3 frames: {self.frames[:3]}\n'
+        s += f'    next 3 write_times: {self.write_times[:3]}\n'
+        s += f'    next_write: {self.next_write}\n'
+        return s
 
     def generate_traffic(self, rate):
         # Create uniform distribution as an array:
@@ -53,12 +61,11 @@ class App:
         X = (-1 / rate) * np.log(1 - U)
 
         # Convert to lists for better functionality:
-        self.frames = list(scale_values(U))
-        self.write_times = list(scale_values(X))
-        self.next_write = 0.0
+        self.frames = [int(i) for i in scale_values(U)]
+        self.write_times = [i for i in X]
 
-    def buffer_frame(self, now):
-        if any(self.frames) and self.next_write <= now:
+    def try_buffer_frame(self, slot):
+        if any(self.frames) and self.next_write <= slot:
             self.station.buffer.append(self.frames.pop(0))
             self.next_write += self.write_times.pop(0)
 
@@ -68,73 +75,97 @@ class Station:
         self.domain: CollisionDomain = None
         self.access_pt: AccessPoint = None
         self.buffer: list = []
-        self.difs: int = DIFS
-        self.trans: dict = {'to': None, 'from': None, 'data': None}
-        self.backoff: int = randint(0, CW)
+        self.difs = DIFS
+        self.backoff = randint(0, CW)
+        self.sifs: int = 0
+        self.freeze_time: int = 0
+        self.transmission: int = 0
+        self.collisions: int = 0
         self.cw: int = 0
+        self.sending: bool = False
         self.awaiting_ack: bool = False
+        self.successes: int = 0
 
-    def sense_channel(self):
-        if len(self.domain.trans) > 1:
-            return
-        elif self.domain.trans[0]['data']:
-            self.trans = self.domain.trans[0]
-            self.collisions = self.cw = 0
-
-    def freeze(self):
-        if self.trans['data']:
-            self.trans -= 1
-        else:
-            self.difs = DIFS
-
-    def transmit(self):
-        self.trans['to'] = self.access_pt
-        self.trans['from'] = self
-        self.trans['data'] = self.buffer[0]
-        self.domain.trans.append(self.trans)
-        self.awaiting_ack = True
+    def state(self):
+        s = f'{self}:\n'
+        s += f'    domain: {self.domain}\n'
+        s += f'    access_pt: {self.access_pt}\n'
+        s += f'    buffer first 3: {self.buffer[:3]}\n'
+        s += f'    difs: {self.difs}\n'
+        s += f'    backoff: {self.backoff}\n'
+        s += f'    sifs: {self.sifs}\n'
+        s += f'    wait: {self.freeze_time}\n'
+        s += f'    collisions: {self.collisions}\n'
+        s += f'    cw: {self.cw}\n'
+        s += f'    sending: {self.sending}\n'
+        s += f'    awaiting_ack: {self.awaiting_ack}\n'
+        s += f'    successes: {self.successes}\n'
+        return s
 
     def double_cw(self):
+        self.sending = False
+
         if self.cw <= CW_MAX:
             self.cw = CW * 2**self.collisions
 
-        # Backoff fool...
         self.backoff = randint(0, self.cw)
         self.collisions += 1
+
+    def freeze(self):
+        if self.freeze_time:
+            self.freeze_time -= 1
+        else:
+            self.freeze_time = self.domain.nav
+            self.difs = DIFS
+
+    def try_send(self):
+        if not self.sending:
+            self.sending = True
+            self.domain.nav = self.transmission = self.buffer[0] + SIFS + ACK
+            self.domain.transmissions += 1
+
+        if self.transmission > 0:
+            self.transmission -= 1
+        else:
+            self.access_pt.sifs = SIFS
+            self.access_pt.ack = ACK
+            self.awaiting_ack = True
 
 
 class AccessPoint:
     def __init__(self):
-        self.domain: CollisionDomain = None
-        self.trans: dict = {'to': None, 'from': None, 'data': None}
-        self.sifs: int = SIFS
+        self.sifs: int = 0
+        self.ack: int = 0
 
-    def decrement_sifs(self):
-        self.sifs -= 1
+    def state(self):
+        s = f'{self}:\n'
+        s += f'    sifs: {self.sifs}\n '
+        return s
 
-    def ack(self):
-        self.domain.trans[0]['to'] = self.domain['from']
-        self.domain.trans[0]['from'] = self
-        self.domain.trans[0]['data'] = ACK
+    def try_ack(self, station):
+        if self.sifs:
+            self.sifs -= 1
+        elif self.ack:
+            self.ack -= 1
+        else:
+            station.sending = False
+            station.domain.nav = 0
+            station.domain.transmissions -= 1
+            station.awaiting_ack = False
+            del station.buffer[0]
+            station.successes += 1
 
 
-@dataclass
 class CollisionDomain:
-    nav: list = []
+    def __init__(self):
+        self.transmissions: int = 0
+        self.nav: int = 0
 
-    def clear(self):
-        self.trans = []
-
-
-def example_poisson():
-    sample = np.random.poisson(5, 100)
-    print('Sample data:')
-    print(sample)
-
-    plot = sb.displot(data=sample, kind='hist', bins=14)
-    plot.set(title='Poisson Distribution', xlabel='event', ylabel='count')
-    pyplot.tight_layout()
-    pyplot.show()
+    def state(self):
+        s = f'{str(self.__class__.__name__)}:\n'
+        s += f'    transmissions: {self.transmissions}\n'
+        s += f'    nav: {self.nav}\n'
+        return s
 
 
 def scale_values(array: np.array) -> np.array:
@@ -150,6 +181,29 @@ def scale_values(array: np.array) -> np.array:
         p += 1
 
     return np.round(array * 10**p)  # apply power of 10 and round
+
+
+def simulation_state(slot, apps, stations, domain):
+    print('Iteration:', slot)
+
+    for station in stations:
+        print(station.state())
+
+    for app in apps:
+        print(app.state())
+
+    print(domain.state())
+
+
+def example_poisson():
+    sample = np.random.poisson(5, 100)
+    print('Sample data:')
+    print(sample)
+
+    plot = sb.displot(data=sample, kind='hist', bins=14)
+    plot.set(title='Poisson Distribution', xlabel='event', ylabel='count')
+    pyplot.tight_layout()
+    pyplot.show()
 
 
 def main():
@@ -173,8 +227,6 @@ def main():
         station.access_pt = access_pt
         station.domain = domain  # to-do: this will change for hidden terminals
 
-    access_pt.domain = domain
-
     # Create app traffic:
     for app in apps:
         app.generate_traffic(ARRIVAL_RATE[0])  # to-do: iterate
@@ -184,92 +236,45 @@ def main():
     end = start + SIM_TIME  # start time plus simulation time
     slot = 0                # slot counter
 
-    print('Simulation start:', start)
-
-    # From my understanding of the directions, we are to time the simulation
-    # but not actually use time to dictate the transmissions. They are just
-    # supposed to flow along according to the slot and then end after 10
-    # seconds. This means that each iteration of the main component of the
-    # script needs to equal one slot. This allows the activities between the
-    # simulated entities to occur in a more realistic way. The only other way
-    # to achieve this parallelism is to have multi-threading, which is not the
-    # objective:
+    print(f'Simulation start: {start}\n')
 
     while True:
         now = time.time()  # get time every iteration
 
-        # Exit the loop if time equals end time:
-        if now >= end:
+        if now >= end:     # exit loop if current time equals end time
             break
 
         slot += 1
 
-        # Stack frames onto station buffer
+        # Stack frames onto station buffer:
         for app in apps:
-            app.buffer_frame(now - end + SIM_TIME)
+            app.try_buffer_frame(now - end + SIM_TIME)
 
         for station in stations:
-            if not any(station.buffer):  # nothing for this station to send
-                continue                 # jump to next station
+            if station.domain.transmissions > 1:
+                station.double_cw()
 
-            # Only one of these happen per slot iteration:
-
-            if station.cw:
-                station.backoff -= 1  # to-do: might make this a function
+            elif station.domain.transmissions == 1 and not station.sending:
+                station.freeze()
 
             elif station.difs:
-                station.sense_channel()
-
-            elif station.trans:
-                station.freeze()
+                station.difs -= 1
 
             elif station.backoff:
                 station.backoff -= 1
-                station.sense_channel()
 
             elif station.awaiting_ack:
-                station.awaiting_ack = False
+                access_pt.try_ack(station)
 
-            else:
-                station.transmit()
+            elif any(station.buffer):
+                station.try_send()
 
-        # The following domain checks occur each slot iteration. This section
-        # represents the "transfer through the ether":
+        domain.transmissions = 0
+        domain.nav = 0
 
-        # There is a collision because the domain object has more than one
-        # transmission dict object in its list:
-        if len(domain.trans) > 1:
-            for station in stations:
-                station.double_cw()
-                domain.trans.clear()
+    print(f'Simulation end: {end}\n')
 
-            continue
-
-        # We have decremented out all the transmission data elsewhere. Clear
-        # the transmission from the "ether" and start the SIFS clock:
-        if not domain.trans[0]['data']:
-            domain.trans.clear()
-            # To-do: decrement a SIFS wait time for stations too?
-            access_pt.decrement_sifs() if access_pt.sifs else access_pt.ack()
-
-            continue
-
-        # Each domain host takes note of the domain transmissions:
-        if not access_pt.trans['data']:
-            access_pt.trans = domain.trans[0].copy()
-
-        for station in stations:
-            if not station.trans['data']:
-                station.trans = domain.trans[0].copy()
-
-        # Frames are transmiting through the domain "ether":
-        domain.trans[0]['data'] -= 1
-        access_pt.trans['data'] -= 1
-
-        for station in stations:
-            station.trans['data'] -= 1
-
-    print('Simulation end:', end)
+    simulation_state(slot, apps, stations, domain)
 
     # Placeholder, will eventually generate the graphs after each run:
     # example_poisson()
