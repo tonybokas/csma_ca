46b8572036e8dbe32f61255f0dab405c4bd3cfe4
Tony Bokas <me@tonybokas.com>
2024-11-16 09:40:33 -0500

    Combined pie plots into subplot

--- project2/topology.py
+++ project2/topology.py
@@ -4,11 +4,26 @@ import pandas as pd
 from matplotlib import pyplot
 
 
-# AS classification Create pie charts
+# GRAPH 1 AS CLASSIFICATION PIE CHART
 
 files = ['20150801.as2types.txt', '20210401.as2types.txt']
 
-for f in files:
+figure, plots = pyplot.subplots(1, 2, figsize=(12, 6))
+
+for i, f in enumerate(files):
     df = pd.read_csv(f, sep='|')
-    df.groupby('type').count().plot.pie(y='as', ylabel='count')
-    pyplot.show()
+
+    df.groupby('type').count().plot.pie(
+        ax=plots[i],
+        title='AS Classes ' + f[:4],
+        y='as',
+        ylabel='percent',
+        autopct='%.1f',
+        colors=['dimgrey', 'darkgrey', 'gainsboro'],
+        legend=False
+        )
+
+figure.savefig(f'1_AS_classes.png', dpi=200)
+
+
+# GRAPH 2
