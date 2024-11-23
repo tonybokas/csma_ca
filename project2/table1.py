9528fe0c26da13a5f59beebb605549f79ce0c5fd
Tony Bokas <me@tonybokas.com>
2024-11-22 21:01:01 -0500

    Fixed comments

--- project2/table1.py
+++ project2/table1.py
@@ -24,18 +24,18 @@ g_rank = (g_rank.groupby('as_id').count()           # count connections
           .sort_values('members', ascending=False)  # rank by count
           .reset_index()[:50])                      # only need top 50
 
-# Filter and save all each distinct as_id value that corresponds to each
-# as_id in the top-50 list:
+# Filter and save each distinct as_id value in the original data
+# that corresponds to each as_id in the top-50 list:
 g_rank['conn'] = g_rank.as_id.apply(
     lambda x: set(df[df.provider_as == x].customer_as.to_list() +
                   df[df.customer_as == x].provider_as.to_list()),
     )
 
-as_id_1 = g_rank.iloc[0]   # the #1 as_id
+as_id_1 = g_rank.iloc[0]  # the #1 as_id
 
 # Check if each as_id in the top 50 is the #1 as_id's connections:
 g_rank['clique_1'] = g_rank.apply(lambda x: x.as_id in as_id_1.conn, axis=1)
-g_rank.loc[0, 'clique_1'] = True  # set to True since its the root
+g_rank.loc[0, 'clique_1'] = True  # set to True since # as_id the root
 
 # Save to CSV:
 g_rank[['as_id', 'members', 'clique_1']].to_csv('5_AS_clique.csv')
